755421ce8b7733def0e78f8bb6773ba17d1390c1
Luigi Lupini <luigi.lupini@gmail.com>
2023-03-30 09:22:23 +0200

    4) Create an about page `app/about/page.jsx`

---
+++ src/app/about/page.jsx
@@ -0,0 +1,39 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium">About</h1>
+      <p>
+        IMDb (Internet Movie Database) is an online database of
+        information related to films, television series, podcasts,
+        home videos, video games, and streaming content online –
+        including cast, production crew and personal biographies, plot
+        summaries, trivia, ratings, and fan and critical reviews. IMDb
+        began as a fan-operated movie database on the Usenet group in
+        1990, and moved to the Web in 1993.
+      </p>
+      <h2 className="text-2xl font-medium">Features</h2>
+      <p>
+        The title and talent pages of IMDb are accessible to all
+        users, but only registered and logged-in users can submit new
+        material and suggest edits to existing entries. Most of the
+        site data has been provided by these volunteers. Registered
+        users with a proven track record are able to add and make
+        corrections to cast lists, credits, and some other data
+        points. However, the addition and removal of images, and
+        alterations to titles, cast and crew names, character names,
+        and plot summaries are subject to an approval process; this
+        usually takes between 24 and 72 hours.
+      </p>
+      <h2>User ratings of films</h2>
+      <p>
+        A s one adjunct to data, the IMDb offers a rating scale that
+        allows users to rate films on a scale of one to ten. IMDb
+        indicates that submitted ratings are filtered and weighted in
+        various ways to produce a weighted mean that is displayed for
+        each film, series, and so on. It states that filters are used
+        to avoid ballot stuffing; the method is not described in
+        detail to avoid attempts to circumvent it.
+      </p>
+    </div>
+  );
+}
